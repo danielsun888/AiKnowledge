bdbd00e2a1231ef008fb3205e7ed07a4bb0fef9d
danielsun888 <81004582+danielsun888@users.noreply.github.com>
2022-12-12 15:47:58 +0900

    presentation

--- mysite_delete/static/hello.js
+++
@@ -1,5 +0,0 @@
-var txt = '{"name":"John", "age":30, "city":"New York"}';
-var obj = JSON.parse(txt);
-console.log(txt);
-var myJSON = JSON.stringify(obj);
-console.log(obj);
--- mysite_delete/users2/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import User2 as  User
-
-admin.site.register(User)
